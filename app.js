703a18e25af6b66341d39fefb94a618a477cc368
Ashutosh Kumar <forjeeprep@gmail.com>
2022-04-15 12:18:23 +0530

    Changes yes again

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const fileUpload = require('express-fileupload')
-const session = require('cookie-session')
+const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const flash = require('connect-flash')
+const MongoStore = require('connect-mongo')
+
 
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -17,6 +19,9 @@ app.use(expressLayouts)
 app.use(cookieParser('ReadingBlogSecure'))
 app.use(session({
     secret: 'ReadingBlogSecretSession',
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://pandaLife:OveMN01tzZCMiuV3@cluster0-shard-00-00.kwxqz.mongodb.net:27017,cluster0-shard-00-01.kwxqz.mongodb.net:27017,cluster0-shard-00-02.kwxqz.mongodb.net:27017/Blogs?ssl=true&replicaSet=atlas-ygx0pm-shard-0&authSource=admin&retryWrites=true&w=majority'
+    }),
     saveUninitialized: true,
     resave: true
 }))
