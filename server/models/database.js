bbc074a27f6b7383ec89f340801eefe404df0a03
Ashutosh Kumar <97580124+incognion@users.noreply.github.com>
2024-08-25 00:27:45 +0530

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery',false)
 const db = async ()=>{
     try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true})
+        const conn = await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        dbName: 'Blogs'
+        })
         console.log(`Database Connected: ${conn.connection.host}`)
     } catch (error){
         console.log(error)
